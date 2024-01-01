5a14b2b649ef4eb5a8f05f675ef0664c2dd35aed
Paul Wood <pmwood@pm.me>
2024-01-01 13:57:22 -0800

    initial tokio

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::io;
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:6142").await?;
+
+    loop {
+        let (mut socket, _) = listener.accept().await?;
+
+        tokio::spawn(async move {
+            println!("Accepted connection from {:?}", socket.peer_addr().unwrap());
+        });
+    }
 }
